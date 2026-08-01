acfd5df1779a78c9761521854dd9a23fa0ad7889
mxwiser <neo@qvqol.com>
2026-08-01 10:02:32 +0800

    up

--- ETH/rmii2axis.sv
+++ ETH/rmii2axis.sv
@@ -5,11 +5,35 @@ module rmii2axis(
     input	wire			rmii_clk,						
 	input	wire			rmii_crs_dv,
 	input	wire	[1:0]	rmii_rxdata,
-	output	reg				rmii_txen,
+	output	reg			    rmii_txen,
 	output	wire	[1:0]	rmii_txdata,
 	output	wire			rmii_rst,
-    axis.master             m_rmii_axis_net
+    axis.master             m_rmii_rx_axis_net,
+    axis.slave              s_rmii_tx_axis_net
 );
+
+localparam		IDLE					= 18'h0_0001,
+                PREAMBLE				= 18'h0_0002,
+                SFD				        = 18'h0_0003,
+                DATA				    = 18'h0_0004,					
+                CRS_CHECK			    = 18'h0_0005;				
+
+wire      rx_dv;
+reg [7:0] state;
+assign m_rmii_rx_axis_net.tlast = rx_dv;
+
+//rx
+always_ff @(posedge rmii_clk or negedge rstn) begin
+    if(rstn==0)begin
+        m_rmii_rx_axis_net.tdata  =  'd0;
+        m_rmii_rx_axis_net.tuser  =  'd0;
+        m_rmii_rx_axis_net.tvalid = 'd0;
+        rx_dv = 'd0;
+        state = 'd0;
+    end else begin
+
+    end
+end
 
 
 endmodule
