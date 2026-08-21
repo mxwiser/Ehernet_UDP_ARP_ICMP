4e6ced73ab0086e26c39cd2e923b18b87c5d1c03
mxwiser <neo@qvqol.com>
2026-08-21 09:50:52 +0800

    up

--- user_logic/HC595PWM.sv
+++ user_logic/HC595PWM.sv
@@ -6,6 +6,31 @@ module HC595PWM #(
     input  logic   clk,
     hc595  hc595_serial
 );
+logic   stcp;
+logic   shcp;
+logic    oen;
+logic     ds;
+
+assign hc595_serial.stcp = stcp;
+assign hc595_serial.shcp = shcp;
+assign hc595_serial.oen  =  oen;
+assign hc595_serial.ds   =   ds;
+
+
+always_ff @(posedge clk or negedge rstn)begin
+    if(!rstn) begin
+        stcp =0;
+        shcp =0;
+        oen  =1;
+        ds   =0;
+    end else begin
+
+    end
+end
+
+
+
+
 /**
 
 **/
